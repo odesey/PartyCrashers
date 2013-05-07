c96de11cade210d40273082d31b840acf3ccaf28
odesey <odesey@gmail.com>
2013-05-07 13:44:56 -0400

    Generated the initial PartyCrashers project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyCrashers::Application.config.secret_token = '02afa59b83d5e541f82a1e1e3f79281d0e979e9fdfcd60bf3cd605abee59a5a628ee9b26840af68c83496fb0300fa2cfa77d92cee21bf4a0e34c10bb1079fc3e'
