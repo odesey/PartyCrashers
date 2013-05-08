d76e766b7d9da7e7e3a6925672a350bed4203315
Joaquin Gamboa <gamboanj@gmail.com>
2013-05-07 22:16:13 -0400

    Added comment controller action and view/partials

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
 
     def create
-       
+        @comment = Comment.new(params[:comment])
+        @comment.save!
+        render @coment
     end
 end
