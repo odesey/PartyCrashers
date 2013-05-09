0dc008acf0e6d6840b1bd0ecd83ec5fc53baa3a1
odesey <odesey@gmail.com>
2013-05-08 20:57:13 -0400

    Added google maps to the index page

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,39 +1,11 @@
-    function initialize() {
-
-
-      var map = new google.maps.Map(document.getElementById("map-canvas"), {
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-      });
-
-      var markerBounds = new google.maps.LatLngBounds();
-      var markersArray = [];
-      var infowindow = new google.maps.InfoWindow();
-
-      <% @region.restaurants.each do |restaurant| %>
-        
-        var myLatlng = new google.maps.LatLng(<%=restaurant.lat%>, <%=restaurant.long%>);
-        markerBounds.extend(myLatlng);
-      
-        var content_string = 
-        '<h1><%=restaurant.name%></h1>'+
-        '<p><%=restaurant.address%><p>';
-
-        var marker = new google.maps.Marker({
-          position: myLatlng,
-          map: map,
-          title:"<%=restaurant.name%>",
-          content: content_string  // custom variable
-        });
-
-        markersArray.push(marker);
-             
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(this.content);
-          infowindow.open(map, this);
-        });
-
-      <% end %>
-
-      map.fitBounds(markerBounds);
-
+$(document).ready(function() {
+  function initialize() {
+    var mapOptions = {
+      center: new google.maps.LatLng(40.7143528, -74.00597309999999),
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+        mapOptions);
+  };
+});
