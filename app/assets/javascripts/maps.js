19bcf2658a93df3d482b4cdb6ebfcc6a85419bfc
Ed <odesey@gmail.com>
2013-05-13 08:09:01 -0400

    added custom images code for google maps....disabled by default

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -4,6 +4,7 @@ var marker;
 var partyPointer;
 var latitude;
 var longitude;
+var iterator = 0;
 
 function initialize() {
 
@@ -11,7 +12,7 @@ function initialize() {
   var myCurrentLocation = new google.maps.LatLng(40.7399977, -73.9900976);
 
   var mapOptions = {
-    zoom: 11,
+    zoom: 12,
     center: myCurrentLocation,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   }
@@ -27,13 +28,50 @@ function initialize() {
   });
 };
 
+function drop() {
+  for (var i =0; i < zmarkerArray.length; i++) {
+    setTimeout(function() {
+      addMarkerMethod();
+    }, i * 2000);
+  }
+}
+
 function partyMarker(latitude, longitude, title) {
+
+  var image = {
+    url: 'http://images.colourbox.com/thumb_COLOURBOX6454919.jpg',
+    // This marker is 20 pixels wide by 32 pixels tall.
+    size: new google.maps.Size(20, 32),
+    // The origin for this image is 0,0.
+    origin: new google.maps.Point(0,0),
+    // The anchor for this image is the base of the flagpole at 0,32.
+    anchor: new google.maps.Point(0, 32)
+  };
+  var shadow = {
+    url: 'images/beachflag_shadow.png',
+    // The shadow image is larger in the horizontal dimension
+    // while the position and offset are the same as for the main image.
+    size: new google.maps.Size(37, 32),
+    origin: new google.maps.Point(0,0),
+    anchor: new google.maps.Point(0, 32)
+  };
+  // Shapes define the clickable region of the icon.
+  // The type defines an HTML &lt;area&gt; element 'poly' which
+  // traces out a polygon as a series of X,Y points. The final
+  // coordinate closes the poly by connecting to the first
+  // coordinate.
+  var shape = {
+      coord: [1, 1, 1, 20, 18, 20, 18 , 1],
+      type: 'poly'
+  };
  var partyPointer = new google.maps.LatLng(latitude,longitude, title);
     console.log(longitude, latitude, title)
     var marker = new google.maps.Marker({
     position: partyPointer,
     map: map,
     draggable:true,
+    // icon: image,
+    shape: shape,
     animation: google.maps.Animation.DROP,
     title: title
     }); 
@@ -46,3 +84,5 @@ function toggleBounce() {
     marker.setAnimation(google.maps.Animation.BOUNCE);
   };
 };
+
+
